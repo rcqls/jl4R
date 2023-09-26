44d0aad427abfc17e4a9b8689e7b0330e031917f
R cqls <rdrouilh@gmail.com>
2023-09-26 09:53:52 +0200

    Update install.sh

--- inst/install.sh
+++ inst/install.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-export JULIA_DIR=$(julia -e "print(joinpath(splitpath(Sys.BINDIR)[1:end-1]))")
+export JULIA_DIR=$(julia -e "p=joinpath(splitpath(Sys.BINDIR)[1:end-1]);print(Sys.iswindows() ? replace(p, Base.Filesystem.path_separator => '/') : p)")
 Rscript -e 'remotes::install_github("rcqls/jl4R",force=TRUE,build=FALSE)'
