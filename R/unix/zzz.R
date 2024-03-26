baa25622ed18e0f1d3762d69a062fa2f41fcc0e6
R cqls <rdrouilh@gmail.com>
2024-03-26 04:00:05 +0100

    fix jl

--- R/unix/zzz.R
+++ R/unix/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(lib, pkg) {
   ## Use local=FALSE to allow easy loading of Tcl extensions
   library.dynam("jl4R", pkg, lib)#,local=FALSE)
-  assignInMyNamespace("JL",.jlenv())
+  assign("jl",.jlenv(),envir=globalenv())
 }
