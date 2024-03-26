0e115872ce4ed0f4dbf0478413934863d8cf24ff
R cqls <rdrouilh@gmail.com>
2024-03-26 05:21:33 +0100

    print jlenv

--- R/jl_env.R
+++ R/jl_env.R
@@ -31,3 +31,7 @@
 `names.jlenv` <- function(obj) {
     setdiff(R(jl("names(Main)")), c("Base","Core","Main","display_buffer","jl4R_ANSWER","preserved_refs"))
 }
+
+`print.jlenv` <- function(obj, ...) {
+    cat("julia environment\n")
+}
