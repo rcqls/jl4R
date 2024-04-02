68a24465db0f7b04c82b16052c033f607f617db8
R cqls <rdrouilh@gmail.com>
2024-04-02 07:38:14 +0200

    Some renaming

--- R/jlR_vector.R
+++ R/jlR_vector.R
@@ -1,15 +1,3 @@
-length.Array <- function(jlval) {
-    jlcallR("length",jlval)
-}
-
-"[.Array" <- function(jlval,i) {
-    s <- length(jlval)
-    if(i > 0 && i <= s) {
-        i <- jl(as.character(i))
-        jlcall("getindex",jlval, i)
-    } else NULL
-}
-
 jl.double <- jl.integer <- jl.logical <- function(obj, vector=FALSE) {
     if(!.jlrunning()) .jlinit()
     jlval <- .Call("jl4R_VECSXP_to_jl_array_EXTPTRSXP", obj, PACKAGE = "jl4R")
---
+++ R/jlValue_Array.R
@@ -0,0 +1,11 @@
+length.Array <- function(jlval) {
+    jlcallR("length",jlval)
+}
+
+"[.Array" <- function(jlval,i) {
+    s <- length(jlval)
+    if(i > 0 && i <= s) {
+        i <- jl(as.character(i))
+        jlcall("getindex",jlval, i)
+    } else NULL
+}
