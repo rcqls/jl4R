91974b3304cc683b676faf8f86bb93380db149c7
R cqls <rdrouilh@gmail.com>
2024-05-06 17:19:05 +0200

    Update jlR_vector.R

--- R/jlR_vector.R
+++ R/jlR_vector.R
@@ -1,7 +1,7 @@
 as_jlvalue.double <- as_jlvalue.integer <- as_jlvalue.logical <- as_jlvalue.character <-function(obj, vector=FALSE, ...) {
     if(!.jlrunning()) .jlinit()
     jlval <- .Call("jl4R_VECSXP_to_jl_array_EXTPTRSXP", obj, PACKAGE = "jl4R")
-    if(length(obj) == 1 && !vector) {
+    if(length(obj) == 1 && !vector && is.null(dim(obj))) {
         jlval[1]
     } else {
         if(is.null(dim(obj))) {
