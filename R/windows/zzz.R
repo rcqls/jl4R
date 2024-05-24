e2ae703f3b71782a0f6f7c396a49adbe4d61a955
R cqls <rdrouilh@gmail.com>
2024-05-24 12:12:13 +0200

    Update zzz.R

--- R/windows/zzz.R
+++ R/windows/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(lib, pkg) {
   ## Use local=FALSE to allow easy loading of Tcl extensions
-  library.dynam("jl4R", pkg, lib,local=FALSE)
+  library.dynam("jl4R", pkg, lib) #,local=FALSE)
   assign("jl",.jlEnv(), envir=globalenv())
 }
