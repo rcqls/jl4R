235b10129e286641ce99471cbd364b32052e052a
R cqls <rdrouilh@gmail.com>
2024-06-07 19:19:52 +0200

    Update JL4R.jl

--- inst/julia/JL4R.jl
+++ inst/julia/JL4R.jl
@@ -7,4 +7,29 @@ function display_buffer(res)
     String(take!(buf))
 end
 
+function funcfind(name)
+    r = Main
+    ns = split(name, ".")
+    for n in ns
+        r = getfield(r, Symbol(n))
+    end
+    r
+end
+
+function safe_call(fname, args...; kwargs...)
+    try
+        # if endswith(fname, ".")
+        #     fname = chop(fname);
+        #     # f = eval(Main, parse(fname));
+        #     f = funcfind(fname);
+        #     r = broadcast(f, args...);
+        # else
+            f = funcfind(fname);
+            f(args...; kwargs...);
+        # end
+    catch e
+        showerror(stdout,e)
+    end;
+end
+
 end
